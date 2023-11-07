f3e7698d501bf0a95243e25f3b50736b78a7a5ee
Adem <benammaradem83@gmail.com>
2023-11-07 11:38:55 +0100

    create new component use angular CLI

--- Projet/src/app/app.module.ts
+++ Projet/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { coursesComponent } from './courses.component';
+import { CoursComponent } from './cours/cours.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    coursesComponent
+    coursesComponent,
+    CoursComponent
      
   ],
   imports: [
